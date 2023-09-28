f9b3ca1b604ab8e8a614a30e598c70422224f7f9
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-09-28 23:05:36 +0200

    feat(client/misc): misc perms

--- client/menus/misc/main.lua
+++ client/menus/misc/main.lua
@@ -29,6 +29,160 @@ end
 
 --#endregion Variables
 
+--#region Functions
+
+function CreateMiscMenu()
+    local perms = lib.callback.await('bMenu:server:hasConvarPermission', false, 'Misc', {'TeleportOptions', 'DevTools', 'ConnectionOptions', 'Menu_Position', 'Toggle_PM', 'Show_Speed', 'Display_Location', 'Show_Time', 'Toggle_Join_Notifications', 'Toggle_Quit_Notifications', 'Toggle_Death_Notifications', 'Toggle_Night_Vision', 'Toggle_Thermal_Vision', 'Toggle_Player_Names'})
+    local menuOptions = {
+        {label = 'No access', description = 'You don\'t have access to any options, press enter to return', args = {'bMenu_main'}}
+    }
+    local index = 1
+
+    if perms.TeleportOptions then
+        menuOptions[index] = {label = 'Teleport Options', args = {'bMenu_misc_options_teleport_options'}}
+        index += 1
+    end
+
+    if perms.DevTools then
+        menuOptions[index] = {label = 'Development Tools', args = {'bMenu_misc_options_developer_options'}}
+        index += 1
+    end
+
+    if perms.ConnectionOptions then
+        menuOptions[index] = {label = 'Connection Options', args = {'bMenu_misc_options_connection_options'}}
+        index += 1
+    end
+
+    if perms.Menu_Position then
+        menuOptions[index] = {label = 'Menu Position', values = {'Top Left', 'Top Right', 'Bottom Left', 'Bottom Right'}, defaultIndex = menuPositionIndexes[MenuPosition], args = {'menu_position'}, close = false}
+        index += 1
+    end
+
+    if perms.Toggle_PM then
+        menuOptions[index] = {label = 'Enable Private Messages', description = 'If this is enabled, other people can send you a private message via the online players tab', checked = enablePMs, args = {'enable_pms'}, close = false}
+        index += 1
+    end
+
+    if perms.Show_Speed then
+        menuOptions[index] = {label = 'Show Speed KM/h', description = 'Show a speedometer on your screen indicating your speed in KM/h', checked = speedKmh, args = {'speed_kmh'}, close = false}
+        index += 1
+
+        menuOptions[index] = {label = 'Show Speed MPH', description = 'Show a speedometer on your screen indicating your speed in MPH', checked = speedMph, args = {'speed_mph'}, close = false}
+        index += 1
+    end
+
+    if perms.Display_Location then
+        menuOptions[index] = {label = 'Display Location', description = 'Shows your current location and heading, as well as the nearest cross road. WARNING: you should not keep this on at all times as this is a very heavy action', checked = displayLocation, args = {'display_location'}, close = false}
+        index += 1
+    end
+
+    if perms.Show_Time then
+        menuOptions[index] = {label = 'Show Time', description = 'Shows you the current time on screen', checked = showTime, args = {'show_time'}, close = false}
+        index += 1
+    end
+
+    if perms.Toggle_Join_Notifications then
+        menuOptions[index] = {label = 'Show Join Notifications', description = 'Receive notifications when someone joins the server', checked = joinNotifs, args = {'show_join_notifs'}, close = false}
+        index += 1
+    end
+
+    if perms.Toggle_Quit_Notifications then
+        menuOptions[index] = {label = 'Show Quit Notifications', description = 'Receive notifications when someone leaves the server', checked = quitNotifs, args = {'show_quit_notifs'}, close = false}
+        index += 1
+    end
+
+    if perms.Toggle_Death_Notifications then
+        menuOptions[index] = {label = 'Show Death Notifications', description = 'Receive notifications when someone dies or gets killed', checked = deathNotifs, args = {'show_death_notifs'}, close = false}
+        index += 1
+    end
+
+    if perms.Toggle_Night_Vision then
+        menuOptions[index] = {label = 'Toggle Night Vision', description = 'Enable or disable night vision', checked = nightVision, args = {'night_vision'}, close = false}
+        index += 1
+    end
+
+    if perms.Toggle_Thermal_Vision then
+        menuOptions[index] = {label = 'Toggle Thermal Vision', description = 'Enable or disable thermal vision', checked = thermalVision, args = {'thermal_vision'}, close = false}
+        index += 1
+    end
+
+    if perms.Toggle_Player_Names then
+        menuOptions[index] = {label = 'Show Player Names', description = 'Enables or disables players names over their head', checked = playerNames, args = {'show_player_names'}, close = false}
+        index += 1
+    end
+
+    lib.registerMenu({
+        id = 'bMenu_misc_options',
+        title = 'Miscellaneous Options',
+        position = MenuPosition,
+        onClose = function(keyPressed)
+            CloseMenu(false, keyPressed, 'bMenu_main')
+        end,
+        onSelected = function(selected)
+            MenuIndexes['bMenu_misc_options'] = selected
+        end,
+        onCheck = function(selected, checked, args)
+            if args[1] == 'enable_pms' then
+                enablePMs = checked
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Enable Private Messages', description = 'If this is enabled, other people can send you a private message via the online players tab', checked = checked, args = {'enable_pms'}, close = false}, selected)
+            elseif args[1] == 'speed_kmh' then
+                speedKmh = checked
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Show Speed KM/H', checked = checked, args = {'speed_kmh'}, close = false}, selected)
+            elseif args[1] == 'speed_mph' then
+                speedMph = checked
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Show Speed MPH', checked = checked, args = {'speed_mph'}, close = false}, selected)
+            elseif args[1] == 'display_location' then
+                displayLocation = checked
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Display Location', description = 'Shows your current location and heading, as well as the nearest cross road', checked = checked, args = {'display_location'}, close = false}, selected)
+            elseif args[1] == 'show_time' then
+                showTime = checked
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Show Time', description = 'Shows you the current time on screen', checked = checked, args = {'show_time'}, close = false}, selected)
+            elseif args[1] == 'show_join_notifs' then
+                joinNotifs = checked
+                lib.callback.await('bMenu:server:setConvar', false, 'chat_showJoins', checked and 1 or 0, true, true, 'bMenu_misc_options', {label = 'Show Join Notifications', description = 'Receive notifications when someone joins the server', checked = checked, args = {'show_join_notifs'}, close = false}, selected)
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Show Join Notifications', description = 'Receive notifications when someone joins the server', checked = checked, args = {'show_join_notifs'}, close = false}, selected)
+            elseif args[1] == 'show_quit_notifs' then
+                quitNotifs = checked
+                lib.callback.await('bMenu:server:setConvar', false, 'chat_showQuits', checked and 1 or 0, true, true, 'bMenu_misc_options', {label = 'Show Quit Notifications', description = 'Receive notifications when someone leaves the server', checked = checked, args = {'show_quit_notifs'}, close = false}, selected)
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Show Quit Notifications', description = 'Receive notifications when someone leaves the server', checked = checked, args = {'show_quit_notifs'}, close = false}, selected)
+            elseif args[1] == 'show_death_notifs' then
+                deathNotifs = checked
+                lib.callback.await('bMenu:server:setConvar', false, 'bMenu_showDeaths', checked and 1 or 0)
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Show Death Notifications', description = 'Receive notifications when someone dies or gets killed', checked = deathNotifs, args = {'show_death_notifs'}, close = false}, selected)
+            elseif args[1] == 'night_vision' then
+                nightVision = checked
+                SetNightvision(checked)
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Toggle Night Vision', description = 'Enable or disable night vision', checked = checked, args = {'night_vision'}, close = false}, selected)
+            elseif args[1] == 'thermal_vision' then
+                thermalVision = checked
+                SetSeethrough(checked)
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Toggle Thermal Vision', description = 'Enable or disable thermal vision', checked = checked, args = {'thermal_vision'}, close = false}, selected)
+            elseif args[1] == 'show_player_names' then
+                playerNames = checked
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Show Player Names', description = 'Enables or disables players names over their head', checked = checked, args = {'show_player_names'}, close = false}, selected)
+            end
+        end,
+        onSideScroll = function(selected, scrollIndex, args)
+            if args[1] == 'menu_position' then
+                MenuPosition = menuPositionNames[scrollIndex]
+                lib.setMenuOptions('bMenu_misc_options', {label = 'Menu Position', values = {'Top Left', 'Top Right', 'Bottom Left', 'Bottom Right'}, defaultIndex = menuPositionIndexes[MenuPosition], args = {'menu_position'}, close = false}, selected)
+                lib.hideMenu(false)
+                lib.showMenu('bMenu_misc_options', MenuIndexes['bMenu_misc_options'])
+            end
+        end,
+        options = menuOptions
+    }, function(_, _, args)
+        if string.match(args[1], 'bMenu') then
+            if args[1] == 'bMenu_misc_options_teleport_options' then
+                SetupTeleportOptions()
+            end
+            lib.showMenu(args[1], MenuIndexes[args[1]])
+        end
+    end)
+end
+
+--#endregion Functions
+
 --#region Events
 
 AddEventHandler('gameEventTriggered', function(name, args)
@@ -52,95 +206,6 @@ lib.callback.register('bMenu:client:canSendMessage', function()
 end)
 
 --#endregion Callbacks
-
---#region Menu Registration
-
-lib.registerMenu({
-    id = 'bMenu_misc_options',
-    title = 'Miscellaneous Options',
-    position = MenuPosition,
-    onClose = function(keyPressed)
-        CloseMenu(false, keyPressed, 'bMenu_main')
-    end,
-    onSelected = function(selected)
-        MenuIndexes['bMenu_misc_options'] = selected
-    end,
-    onCheck = function(selected, checked, args)
-        if args[1] == 'enable_pms' then
-            enablePMs = checked
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Enable Private Messages', description = 'If this is enabled, other people can send you a private message via the online players tab', checked = checked, args = {'enable_pms'}, close = false}, selected)
-        elseif args[1] == 'speed_kmh' then
-            speedKmh = checked
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Show Speed KM/H', checked = checked, args = {'speed_kmh'}, close = false}, selected)
-        elseif args[1] == 'speed_mph' then
-            speedMph = checked
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Show Speed MPH', checked = checked, args = {'speed_mph'}, close = false}, selected)
-        elseif args[1] == 'display_location' then
-            displayLocation = checked
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Display Location', description = 'Shows your current location and heading, as well as the nearest cross road', checked = checked, args = {'display_location'}, close = false}, selected)
-        elseif args[1] == 'show_time' then
-            showTime = checked
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Show Time', description = 'Shows you the current time on screen', checked = checked, args = {'show_time'}, close = false}, selected)
-        elseif args[1] == 'show_join_notifs' then
-            joinNotifs = checked
-            lib.callback.await('bMenu:server:setConvar', false, 'chat_showJoins', checked and 1 or 0, true, true, 'bMenu_misc_options', {label = 'Show Join Notifications', description = 'Receive notifications when someone joins the server', checked = checked, args = {'show_join_notifs'}, close = false}, selected)
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Show Join Notifications', description = 'Receive notifications when someone joins the server', checked = checked, args = {'show_join_notifs'}, close = false}, selected)
-        elseif args[1] == 'show_quit_notifs' then
-            quitNotifs = checked
-            lib.callback.await('bMenu:server:setConvar', false, 'chat_showQuits', checked and 1 or 0, true, true, 'bMenu_misc_options', {label = 'Show Quit Notifications', description = 'Receive notifications when someone leaves the server', checked = checked, args = {'show_quit_notifs'}, close = false}, selected)
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Show Quit Notifications', description = 'Receive notifications when someone leaves the server', checked = checked, args = {'show_quit_notifs'}, close = false}, selected)
-        elseif args[1] == 'show_death_notifs' then
-            deathNotifs = checked
-            lib.callback.await('bMenu:server:setConvar', false, 'bMenu_showDeaths', checked and 1 or 0)
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Show Death Notifications', description = 'Receive notifications when someone dies or gets killed', checked = deathNotifs, args = {'show_death_notifs'}, close = false}, selected)
-        elseif args[1] == 'night_vision' then
-            nightVision = checked
-            SetNightvision(checked)
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Toggle Night Vision', description = 'Enable or disable night vision', checked = checked, args = {'night_vision'}, close = false}, selected)
-        elseif args[1] == 'thermal_vision' then
-            thermalVision = checked
-            SetSeethrough(checked)
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Toggle Thermal Vision', description = 'Enable or disable thermal vision', checked = checked, args = {'thermal_vision'}, close = false}, selected)
-        elseif args[1] == 'show_player_names' then
-            playerNames = checked
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Show Player Names', description = 'Enables or disables players names over their head', checked = checked, args = {'show_player_names'}, close = false}, selected)
-        end
-    end,
-    onSideScroll = function(selected, scrollIndex, args)
-        if args[1] == 'menu_position' then
-            MenuPosition = menuPositionNames[scrollIndex]
-            lib.setMenuOptions('bMenu_misc_options', {label = 'Menu Position', values = {'Top Left', 'Top Right', 'Bottom Left', 'Bottom Right'}, defaultIndex = menuPositionIndexes[MenuPosition], args = {'menu_position'}, close = false}, selected)
-            lib.hideMenu(false)
-            lib.showMenu('bMenu_misc_options', MenuIndexes['bMenu_misc_options'])
-        end
-    end,
-    options = {
-        {label = 'Teleport Options', args = {'bMenu_misc_options_teleport_options'}},
-        {label = 'Development Tools', args = {'bMenu_misc_options_developer_options'}},
-        {label = 'Connection Options', args = {'bMenu_misc_options_connection_options'}},
-        {label = 'Menu Position', values = {'Top Left', 'Top Right', 'Bottom Left', 'Bottom Right'}, defaultIndex = menuPositionIndexes[MenuPosition], args = {'menu_position'}, close = false},
-        {label = 'Enable Private Messages', description = 'If this is enabled, other people can send you a private message via the online players tab', checked = enablePMs, args = {'enable_pms'}, close = false},
-        {label = 'Show Speed KM/h', description = 'Show a speedometer on your screen indicating your speed in KM/h', checked = speedKmh, args = {'speed_kmh'}, close = false},
-        {label = 'Show Speed MPH', description = 'Show a speedometer on your screen indicating your speed in MPH', checked = speedMph, args = {'speed_mph'}, close = false},
-        {label = 'Display Location', description = 'Shows your current location and heading, as well as the nearest cross road. WARNING: you should not keep this on at all times as this is a very heavy action', checked = displayLocation, args = {'display_location'}, close = false},
-        {label = 'Show Time', description = 'Shows you the current time on screen', checked = showTime, args = {'show_time'}, close = false},
-        {label = 'Show Join Notifications', description = 'Receive notifications when someone joins the server', checked = joinNotifs, args = {'show_join_notifs'}, close = false},
-        {label = 'Show Quit Notifications', description = 'Receive notifications when someone leaves the server', checked = quitNotifs, args = {'show_quit_notifs'}, close = false},
-        {label = 'Show Death Notifications', description = 'Receive notifications when someone dies or gets killed', checked = deathNotifs, args = {'show_death_notifs'}, close = false},
-        {label = 'Toggle Night Vision', description = 'Enable or disable night vision', checked = nightVision, args = {'night_vision'}, close = false},
-        {label = 'Toggle Thermal Vision', description = 'Enable or disable thermal vision', checked = thermalVision, args = {'thermal_vision'}, close = false},
-        {label = 'Show Player Names', description = 'Enables or disables players names over their head', checked = playerNames, args = {'show_player_names'}, close = false}
-    }
-}, function(_, _, args)
-    if string.match(args[1], 'bMenu') then
-        if args[1] == 'bMenu_misc_options_teleport_options' then
-            SetupTeleportOptions()
-        end
-        lib.showMenu(args[1], MenuIndexes[args[1]])
-    end
-end)
-
---#endregion Menu Registration
 
 --#region Threads
 
